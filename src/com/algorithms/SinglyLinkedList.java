6968952a3a35016ba2c2aa9a502150e2ef42cfc3
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-03 22:18:19 -0600

    finished insert while sorting method

--- src/com/algorithms/SinglyLinkedList.java
+++ src/com/algorithms/SinglyLinkedList.java
@@ -1,7 +1,5 @@
 package com.algorithms;
 
-
-
 public class SinglyLinkedList<T extends Comparable<T>> {
 
     Node<T> head;
@@ -14,24 +12,24 @@ public class SinglyLinkedList<T extends Comparable<T>> {
     public void insertNode(T data) {
 
         Node<T> newNode = new Node<>(data);
-        Node<T> currentNode;
-
         if(head == null || newNode.data.compareTo(head.data) <= 0) {
+
+            newNode.nextNode = head;
             head = newNode;
+
         }
         else {
-            currentNode = head;
 
-            while (currentNode.nextNode != null && currentNode.nextNode.data.compareTo(newNode.data) > 0){
-                if(currentNode.data.compareTo(newNode.data) > 0){
-                    currentNode = currentNode.nextNode;
-                    newNode.nextNode = currentNode.nextNode;
-                    currentNode.nextNode = newNode;
-                }
+            Node<T> currentNode = head;
+            Node<T> tempNode = currentNode;
+
+            while (currentNode != null && currentNode.data.compareTo(newNode.data) < 0) {
+                tempNode = currentNode;
+                currentNode = currentNode.nextNode;
             }
 
-
-
+            newNode.nextNode = currentNode;
+            tempNode.nextNode = newNode;
 
         }
     }
--- src/com/algorithms/SinglyLinkedListDriver.java
+++ src/com/algorithms/SinglyLinkedListDriver.java
@@ -4,17 +4,26 @@ package com.algorithms;
 public class SinglyLinkedListDriver {
 
     public static void main(String[] args) {
-        SinglyLinkedList<String> list = new SinglyLinkedList<>();
 
-        list.insertNode("Z");
-        list.insertNode("C");
-        list.insertNode("B");
-        list.insertNode("B");
 
-        Node temp = list.head;
-        while(temp != null){
-            System.out.println(temp.data);
-            temp = temp.nextNode;
+
+        SinglyLinkedList<String> list = new SinglyLinkedList();
+
+        list.insertNode("trevor");
+
+        list.insertNode("dave");
+        list.insertNode("john");
+
+        list.insertNode("ziggy");
+
+        list.insertNode("andrew");
+        list.insertNode("andre");
+
+        list.insertNode("cameron");
+
+        while(list.head != null){
+            System.out.println(list.head.data);
+            list.head = list.head.nextNode;
         }
 
 
